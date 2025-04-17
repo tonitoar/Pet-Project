b916bc19479978f460e654f73bdee3e2ba01fb87
Toni Torredemer <toni.torredemer.argiles@gmail.com>
2025-04-17 19:13:01 +0200

    first route created

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import Root from './components/root';
 import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"; 
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Root/>} /> 
+ ));
 
 function App() {
   return (
