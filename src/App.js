c2f3a8b5018388831dd161127def3c69e19c39a8
Toni Torredemer <toni.torredemer.argiles@gmail.com>
2025-04-22 18:14:18 +0200

    Nested Routes and the Index Route => using Index prop + HOMEPAGE page + Outlet component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} fr
 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<Root/>} /> 
+  <Route path="/" element={<Root/>} > 
+    <Route index element={<HomePage/>} /> {/*INDEX (DEFAULT) => RENDER HOMEPAGE JUNT AMB ROOT EN EL PATH "/" */}
+  </Route>
  ));
 
 function App() {
